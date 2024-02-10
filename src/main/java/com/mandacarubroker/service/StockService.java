f74a7e4ad10069794d3530987497b89b6aa0147d
Moisés Oliveira Costa <mosolicos@gmail.com>
2024-02-10 10:17:48 -0300

    corrigindo o StockService e atualizando o readme
    - corrigindo ordem de imports
    - tirando imports com * e colocando só os imports necessários
    - tirando métodos que não sendo utilizados
    - colocando Javadocs nos métodos

--- src/main/java/com/mandacarubroker/service/StockService.java
+++ src/main/java/com/mandacarubroker/service/StockService.java
@@ -3,75 +3,104 @@ package com.mandacarubroker.service;
 import com.mandacarubroker.domain.stock.RequestStockDTO;
 import com.mandacarubroker.domain.stock.Stock;
 import com.mandacarubroker.domain.stock.StockRepository;
-import jakarta.validation.*;
-import org.springframework.stereotype.Service;
-
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import org.springframework.stereotype.Service;
 
+/**
+ * Service for managing stock-related operations.
+ *
+ * @author Ricardo Vilela
+ */
 @Service
 public class StockService {
 
+  private final StockRepository stockRepository;
 
-    private final StockRepository stockRepository;
+  public StockService(StockRepository stockRepository) {
+    this.stockRepository = stockRepository;
+  }
 
-    public StockService(StockRepository stockRepository) {
-        this.stockRepository = stockRepository;
-    }
+  public List<Stock> getAllStocks() {
+    return stockRepository.findAll();
+  }
 
-    public List<Stock> getAllStocks() {
-        return stockRepository.findAll();
-    }
+  public Optional<Stock> getStockById(String id) {
+    return stockRepository.findById(id);
+  }
 
-    public Optional<Stock> getStockById(String id) {
-        return stockRepository.findById(id);
-    }
+  /**
+   * Creates a new stock based on the provided request data.
+   * This method validates the request data using JSR-380 annotations
+   * and throws a `ConstraintViolationException` if there are any errors.
+   * If the data is valid, a new `Stock` object is created and saved to the database.
+   *
+   * @param data The request data containing stock information.
+   * @return The saved `Stock` object.
+   * @throws ConstraintViolationException If the request data is invalid.
+   * @author Ricardo Vilela
+   */
+  public Stock createStock(RequestStockDTO data) {
+    Stock novaAcao = new Stock(data);
+    validateRequestStockDto(data);
+    return stockRepository.save(novaAcao);
+  }
 
-    public Stock createStock(RequestStockDTO data) {
-        Stock novaAcao = new Stock(data);
-        validateRequestStockDTO(data);
-        return stockRepository.save(novaAcao);
-    }
+  /**
+   * Updates an existing stock with the provided information.
+   *
+   * @param id           The ID of the stock to update.
+   * @param updatedStock The updated stock information.
+   * @return An Optional containing the updated stock, or empty if the stock was not found.
+   * @author Ricardo Vilela
+   */
+  public Optional<Stock> updateStock(String id, Stock updatedStock) {
+    return stockRepository.findById(id)
+            .map(stock -> {
+              stock.setSymbol(updatedStock.getSymbol());
+              stock.setCompanyName(updatedStock.getCompanyName());
+              double newPrice = stock.changePrice(updatedStock.getPrice(), true);
+              stock.setPrice(newPrice);
 
-    public Optional<Stock> updateStock(String id, Stock updatedStock) {
-        return stockRepository.findById(id)
-                .map(stock -> {
-                    stock.setSymbol(updatedStock.getSymbol());
-                    stock.setCompanyName(updatedStock.getCompanyName());
-                    double newPrice = stock.changePrice(updatedStock.getPrice(), true);
-                    stock.setPrice(newPrice);
+              return stockRepository.save(stock);
+            });
+  }
 
-                    return stockRepository.save(stock);
-                });
-    }
+  public void deleteStock(String id) {
+    stockRepository.deleteById(id);
+  }
 
-    public void deleteStock(String id) {
-        stockRepository.deleteById(id);
-    }
+  /**
+   * Validates the given RequestStockDTO, ensuring that it meets all validation constraints.
+   *
+   * @param data The RequestStockDTO to validate.
+   * @throws ConstraintViolationException If validation fails.
+   * @author Ricardo Vilela
+   */
+  public static void validateRequestStockDto(RequestStockDTO data) {
+    try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+      Validator validator = factory.getValidator();
+      Set<ConstraintViolation<RequestStockDTO>> violations = validator.validate(data);
 
-    public static void validateRequestStockDTO(RequestStockDTO data) {
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        Validator validator = factory.getValidator();
-        Set<ConstraintViolation<RequestStockDTO>> violations = validator.validate(data);
+      if (!violations.isEmpty()) {
+        StringBuilder errorMessage = new StringBuilder("Validation failed. Details: ");
 
-        if (!violations.isEmpty()) {
-            StringBuilder errorMessage = new StringBuilder("Validation failed. Details: ");
-
-            for (ConstraintViolation<RequestStockDTO> violation : violations) {
-                errorMessage.append(String.format("[%s: %s], ", violation.getPropertyPath(), violation.getMessage()));
-            }
-
-            errorMessage.delete(errorMessage.length() - 2, errorMessage.length());
-
-            throw new ConstraintViolationException(errorMessage.toString(), violations);
+        for (ConstraintViolation<RequestStockDTO> violation : violations) {
+          errorMessage.append(String.format("[%s: %s], ",
+                  violation.getPropertyPath(), violation.getMessage()));
         }
-    }
 
-    public void validateAndCreateStock(RequestStockDTO data) {
-        validateRequestStockDTO(data);
+        errorMessage.delete(errorMessage.length() - 2, errorMessage.length());
 
-        Stock novaAcao = new Stock(data);
-        stockRepository.save(novaAcao);
+        throw new ConstraintViolationException(errorMessage.toString(), violations);
+      }
     }
+  }
+
 }
